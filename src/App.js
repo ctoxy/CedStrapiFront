28d540f654ac80d316a30bb17ff05675c57f54bf
ctoxy <cedric.bouve@gmail.com>
2021-04-12 14:47:51 +0200

    array of post without strapi

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css'
 import Post from './components/Post';
 
 
-const post = {
-  likes:20,
-  description: "the changed description",
-  image: {
-    url: "/uploads/Capture_358e809b5a.PNG"
-  }
-}
+const posts = [
+  {
+    likes:20,
+    description: "the changed description",
+    image: {
+      url: "/uploads/Capture_358e809b5a.PNG"
+    }
+  }, 
+  {
+    likes:20,
+    description: "the changed description",
+    image: {
+      url: "/uploads/Capture_358e809b5a.PNG"
+      }
+  },
+]
 function App() {
   return (
     <div className="App">
+     { posts.map( post => (
       <Post
         likes={post.likes}
         description={post.description}
         url={post.image && post.image.url}
       />
+     ))}
+      
     
     </div>
         
