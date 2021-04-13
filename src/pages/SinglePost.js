50b8250b7733245fc5a926174c717e6bf4d172be
ctoxy <cedric.bouve@gmail.com>
2021-04-13 16:36:57 +0200

    editing and refresh description on a post ok

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -9,6 +9,21 @@ export default({match, history}) => {
 
     const [post, setPost] = useState({});
     const [loading, setLoading] = useState(true);
+    const [edit, setEdit] = useState(false);
+
+    /*Use for the edit form */
+    const [description, setDescription] = useState('');
+
+    /*methode fetch data */
+    const fetchPost = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`);
+        const data = await response.json();
+
+        console.log("data", data);
+        setPost(data);
+        setDescription(data.description);
+        setLoading(false);
+    }
 
     /*methode DELETE */
     const handleDelete = async () => {
@@ -20,15 +35,30 @@ export default({match, history}) => {
         history.push('/')
 
     }
-    useEffect(() => {
-        const fetchPost = async () => {
-            const response = await fetch(`http://localhost:1337/posts/${id}`);
-            const data = await response.json();
 
-            console.log("data", data);
-            setPost(data);
-            setLoading(false);
-        }
+    /*methode Update*/
+    const handleEditSubmit = async (event) => {
+        event.preventDefault();
+        console.log("handleEditSubmit");
+
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                description
+            })
+        })
+        const data = await response.json();
+        /*permet de rafraichir apres modif */
+        fetchPost();
+
+        console.log("handleEditSubmit data", data);
+
+    }
+    useEffect(() => {
+        
         fetchPost();
         /* [] cidessous est le second param qui evite une requete permanente tres important */
     }, []);
@@ -47,6 +77,18 @@ export default({match, history}) => {
                             likes= {post.likes}
                             />
                             <button onClick={handleDelete}>Delete the post</button>
+                            <button onClick={() => setEdit(true)}>Edit the post</button>
+                            {edit && 
+                                <form onSubmit={handleEditSubmit}>
+                                    <input
+                                        value={description}
+                                        onChange={(event) => setDescription(event.target.value)}
+                                        placeholder="New Description"
+                                    />
+                                    <button >Confirm</button>
+                                
+                                </form>
+                            }
                         </React.Fragment>
                     }
                     {!post.id &&
