b16421f14e2fe36c49be948c2d912d66d06bb916
ctoxy <cedric.bouve@gmail.com>
2021-04-13 11:41:57 +0200

    gestion error message post form

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -8,38 +8,66 @@ export default () => {
     /*appel de state file sur les deux etats a un tableau vide*/
     const [file, setFile ] = useState(null);
     console.log("file", file);
+
+    /*appel de state error sur les deux etats a un tableau vide*/
+    const [error, setError ] = useState('');
+    
     
     /*recueil du formulaire */
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if (!description) {
+            setError('Please add a description')
+            return
+        }
 
+        if (!file) {
+            setError('Please upload an image')
+            return
+        }
         const formData = new FormData();
         formData.append('data', JSON.stringify({description}));
         formData.append('files.image', file);
 
-
-        const response = await fetch('http://localhost:1337/posts', {
+        try {
+            const response = await fetch('http://localhost:1337/posts', {
             method: "POST",            
             body: formData
         })
 
-        const data = await response.json();
-        console.log("data", data);
+            const data = await response.json();
+            console.log("data", data);
+            
+        } catch (err) {
+            console.log("Exeption ", err);
+            setError(err);
+        }
+
+
+        
     }
 
     return (
         <div className="Create">
             <h2>Create</h2>
+
+            {error && <p> { error }</p>}
             <form onSubmit={handleSubmit}>
                 <input 
                     placeholder="description"
                     value={description}
-                    onChange= {(event) => setDescription(event.target.value)}
+                    onChange= {(event) => {
+                        setError('');
+                        setDescription(event.target.value);
+                    }}
                 />
                 <input 
                     placeholder="add a file"
                     type="file"
-                    onChange= {(event) => setFile(event.target.files[0])}
+                    onChange= {(event) => {
+                        setError('');
+                        setFile(event.target.files[0])
+                    }}
                 />
                 <button>Submit</button>
             
