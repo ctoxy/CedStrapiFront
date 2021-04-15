577443b802e73788d0e5cc69a830993f992620c2
ctoxy <cedric.bouve@gmail.com>
2021-04-15 14:11:23 +0200

    Build the. Login Form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React , {useState}from 'react';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+    
+    /*appel de state email sur les deux etats a un tableau vide*/
+    const [email, setEmail ] = useState('');
+    /*appel de state login sur les deux etats a un tableau vide*/
+    const [password, setPassword ] = useState('');
+    
+    /*appel de state error sur les deux etats a un tableau vide*/
+    const [error, setError ] = useState('');
+    
+    
+    /*recueil du formulaire */
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        try {
+            const response = await fetch('http://localhost:1337/auth/local/', {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json'
+            },            
+            body: JSON.stringify({
+                identifier:email,
+                password
+            })
+        })
+
+            const data = await response.json();
+            console.log("data", data);
+            
+        } catch (err) {
+            console.log("Exeption ", err);
+            setError(err);
+        }
+
+
+        
+    }
+
     return (
-        <div>
-            Login
+        <div className="Login">
+            <h2>Login</h2>
+
+            <form onSubmit={handleSubmit}>
+                <input 
+                    placeholder="email"
+                    type="email"
+                    value={email}
+                    onChange= {(event) => {
+                        setError('');
+                        setEmail(event.target.value);
+                    }}
+                />
+                <input 
+                    placeholder="password"
+                    type="password"
+                    value={password}
+                    onChange= {(event) => {
+                        setError('');
+                        setPassword(event.target.value)
+                    }}
+                />
+                <button>Login</button>
+            
+            </form>
         </div>
     )
 }
