92121e0d81bffd6098b5dde1250bcabcf1166615
ctoxy <cedric.bouve@gmail.com>
2021-04-15 14:35:05 +0200

    Redirect on Successful Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,28 @@
-import React , {useState}from 'react';
+import React , {useState, useContext, useEffect}from 'react';
+import { UserContext } from '../context/UserContext';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default ({history}) => {
     
     /*appel de state email sur les deux etats a un tableau vide*/
     const [email, setEmail ] = useState('');
-    /*appel de state login sur les deux etats a un tableau vide*/
+    /* appel de state login sur les deux etats a un tableau vide*/
     const [password, setPassword ] = useState('');
     
-    /*appel de state error sur les deux etats a un tableau vide*/
+    /* appel de state error sur les deux etats a un tableau vide*/
     const [error, setError ] = useState('');
-    
-    
+    /* utilisation du context user pour le login  */
+    const {user, setUser} = useContext(UserContext);
+    console.log('user', user);
+
+    /* appel du use effect pour la fonction afin de savoir si on est loggé ou pas */
+
+    useEffect(() => {
+        if (user) {
+          history.push('/');  
+        } 
+        /* [] cidessous est le second param qui evite une requete permanente tres important */
+    }, [user]);
     /*recueil du formulaire */
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -35,6 +46,7 @@ export default () => {
                 return /*stop execution*/
             }
             
+            setUser(data);
             
         } catch (err) {
             console.log("Exeption ", err);
